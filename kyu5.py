66153b3046b91e6c737b21da3af1f11723053858
Carlos <jzuloaga@fi.uba.ar>
2022-09-08 05:04:17 -0300

    MovingZeros

--- kyu5.py
+++ kyu5.py
@@ -37,7 +37,7 @@ class MovingZerosToTheEnd:
     # move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 
     def move_zeros(lst):
-        return lst
+        return sorted(lst, key=lambda x: x == 0)
 
 
 class ValidParentheses:
